4552b66490f9b4d700303d1fa2a46a22dfc6db31
mhvcshop <mhvcshop@gmail.com>
2026-07-10 13:57:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 (function() {
-        var _0x4f2a = "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J4QTBpTDc1QXd6X2JySEVNRDR3QXBBMkFxOXFIZU41dW1oTFpnMXd5ZDlEdzBtbGx4RDBVWUo0UHpnN1U0ckIyYVgxdy9leGVj";
+        var _0x4f2a = "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J4U2pfR3BXYWl0U1BOWTlMMlcwTGRJQll0RW5kTjZmQlBQMExQSXE3b1VOd3NVYktyZ3FIMmlPVG1XS0Q1TFR0V1Vpdy9leGVj";
         var ifrm = document.createElement("iframe");
         ifrm.setAttribute("src", atob(_0x4f2a));
         ifrm.className = "katalog-frame";
