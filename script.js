adbf8b5ba6a0ce8c5dcf26778c4ec890385b2251
mhvcshop <mhvcshop@gmail.com>
2026-07-10 11:30:04 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+(function() {
+        var _0x4f2a = "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J4QTBpTDc1QXd6X2JySEVNRDR3QXBBMkFxOXFIZU41dW1oTFpnMXd5ZDlEdzBtbGx4RDBVWUo0UHpnN1U0ckIyYVgxdy9leGVj";
+        var ifrm = document.createElement("iframe");
+        ifrm.setAttribute("src", atob(_0x4f2a));
+        ifrm.className = "katalog-frame";
+        ifrm.setAttribute("scrolling", "yes");
+        ifrm.setAttribute("allow", "fullscreen");
+        document.getElementById("wrapper").appendChild(ifrm);
+})();
